e270c67a28e724a94ef6efdf83e10c8fd2371fa4
flavorfan <fanck1979@gmail.com>
2019-08-11 21:13:01 +0700

    8-11 #90

---
+++ 90.subsets-ii.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=90 lang=python3
+#
+# [90] Subsets II
+#
+from typing import List
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        for num in sorted(nums):
+            result += [i+[num] for i in result if i+[num] not in result]
+        return result
+	
+
+        
+if __name__ == '__main__':
+    nums = [1,2,2]
+    sln = Solution()
+    print(sln.subsetsWithDup(nums))
+    pass
