5d0c3a02ab2039f2b2e7d48a2dbf3bc2fe6e5b7c
flavorfan <fanck1979@gmail.com>
2019-08-20 18:15:04 +0800

    update 126

--- 126.word-ladder-ii.py
+++ 126.word-ladder-ii.py
@@ -8,23 +8,29 @@ from typing import List
 import collections
 
 class Solution:
-    # def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-    #     tree, words, n = collections.defaultdict(set), set(wordList), len(beginWord)
-    #     if endWord not in words: return [] 
-    #     found, q, nq = False, {beginWord}, set() 
-    #     while q and not found:
-    #         words -= set(q)
-    #         for x in q:
-    #             for y in [x[:i] + c + x[i+1:] for i in range(n) for c in 'qwertyuiopasdfghjklzxcvbnm' ]:
-    #                 if y in words: 
-    #                     if y == endWord: found = True 
-    #                 else: nq.add(y) 
-    #                 tree[x].add(y) 
-    #         q, nq = nq, set() 
-    #     def bt(x):
-    #         return [[x]] if x == endWord else [[x] + rest for y in tree[x] for rest in bt(y)]
-    #     return bt(beginWord)
+    def findLadders_old(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        tree, words, n = collections.defaultdict(set), set(wordList), len(beginWord)
+        if endWord not in wordList: return []
+        # 1- concreate the search tree 
+        # not "seen" set but a more like "explored" set. 
+        found, q, nq = False, {beginWord}, set()
+        while q and not found:
+            words -= set(q)
+            for x in q:
+                for y in [x[:i]+c+x[i+1:] for i in range(n) for c in 'qwertyuiopasdfghjklzxcvbnm']:
+                    if y in words:
+                        if y == endWord: found = True
+                        else: nq.add(y)
+                        tree[x].add(y)
+            q, nq = nq, set()
+        # 2- bfs
+        def bt(x): 
+            return [[x]] if x == endWord else [[x] + rest for y in tree[x] for rest in bt(y)]
+        return bt(beginWord)    
 
+    # single directional BFS which cost more than 2500ms. 
+    # BFS't time complexity is O(b^d) where b is branch factor and d is depth. 
+    # bi-directional BFS reducing running time from 2500ms to 100ms!
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
         tree, words, n = collections.defaultdict(set), set(wordList), len(beginWord)
         if endWord not in wordList: return []
@@ -34,7 +40,7 @@ class Solution:
             for x in bq:
                 for y in [x[:i]+c+x[i+1:] for i in range(n) for c in 'qwertyuiopasdfghjklzxcvbnm']:
                     if y in words:
-                        if y in eq: found = True
+                        if y in eq: found = True    # important 
                         else: nq.add(y)
                         tree[y].add(x) if rev else tree[x].add(y)
             bq, nq = nq, set()
@@ -48,5 +54,5 @@ if __name__ == '__main__':
     endWord = "cog"
     wordList = ["hot","dot","dog","lot","log","cog"]
     sln = Solution()
-    print(sln.findLadders(beginWord,endWord,wordList))
+    print(sln.findLadders_old(beginWord,endWord,wordList))
     pass 
