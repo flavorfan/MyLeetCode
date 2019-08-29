38ac54e438a3d1de628305e1c0ad627557193c1a
flavorfan <fanck1979@gmail.com>
2019-08-29 19:37:12 +0800

    update: more effective solution

--- 677.map-sum-pairs.py
+++ 677.map-sum-pairs.py
@@ -3,7 +3,7 @@
 #
 # [677] Map Sum Pairs
 #
-class MapSum:
+class MapSum_my:
 
     def __init__(self):
         """
@@ -42,6 +42,53 @@ class MapSum:
                 return 0 
             curr = curr[ch]
         return curr['-']
+
+class Node(object):
+    def __init__(self, val=0):
+        self.val = val
+        self.next = {}
+
+class MapSum(object):
+    
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = Node()
+        self.keys = {}          # important
+        
+    def insert(self, key, val):
+        """
+        :type key: str
+        :type val: int
+        :rtype: void
+        """
+        node = self.root
+        i = 0
+        while i < len(key):
+            # add following nodes
+            if key[i] not in node.next:
+                node.next[key[i]] = Node()
+            node = node.next[key[i]]
+            # update existed key-value pair
+            if key in self.keys:
+                node.val += val - self.keys[key]
+            else:
+                node.val += val
+            i += 1
+        self.keys[key] = val
+        
+    def sum(self, prefix):
+        """
+        :type prefix: str
+        :rtype: int
+        """
+        node = self.root
+        i = 0
+        while i < len(prefix) and prefix[i] in node.next:
+            node = node.next[prefix[i]]
+            i += 1
+        return node.val if i == len(prefix) else 0
 
 if __name__ == '__main__':
     mapsum = MapSum()
