e1d6f230a5e10ed26c193d28e7c5e659fa81340e
flavorfan <fanck1979@gmail.com>
2019-11-18 20:15:51 +0800

    11-18 # 310

--- 310.minimum-height-trees.py
+++ 310.minimum-height-trees.py
@@ -8,7 +8,7 @@
 from typing import List
 import collections
 class Solution:
-    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+    def findMinHeightTrees_2(self, n: int, edges: List[List[int]]) -> List[int]:
         if n <= 1:
             return [0]
         graph = collections.defaultdict(list)
@@ -30,6 +30,19 @@ class Solution:
                         tmp.append(nei)
             queue = tmp
         return ans
+    # High level idea is to keep pruning the outer node which has zero or one edge from the tree.
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        tree = [set() for _ in range(n)]
+        for u, v in edges: tree[u].add(v), tree[v].add(u)
+        q, nq = [x for x in range(n) if len(tree[x]) < 2], []
+        while True:
+            for x in q:
+                for y in tree[x]:
+                    tree[y].remove(x)
+                    if len(tree[y]) == 1: nq.append(y)
+            if not nq: break
+            nq, q = [], nq
+        return q
 # @lc code=end
 
 if __name__ == '__main__':
