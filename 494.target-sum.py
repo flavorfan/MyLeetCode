d0af2ee65fa6f1309c5d4154d454686698990581
flavorfan <fanck1979@gmail.com>
2019-11-28 22:12:28 +0800

    11-28 $ 513

--- 494.target-sum.py
+++ 494.target-sum.py
@@ -4,6 +4,7 @@
 # [494] Target Sum
 #
 from typing import List
+import collections
 # @lc code=start
 class Solution:
     def findTargetSumWays_1(self, nums: List[int], S: int) -> int:
@@ -13,7 +14,7 @@ class Solution:
             return d.get((i, cur), int(cur == S))
         
         return dfs(0, 0)
-    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+    def findTargetSumWays_2(self, nums: List[int], S: int) -> int:
         def dfs(i, my_sum, cache={}):
             if (i, my_sum) not in cache:
                 if i == len(nums):
@@ -23,6 +24,17 @@ class Solution:
             return cache[(i, my_sum)]
 
         return dfs(0, 0)
+
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+        if not nums or sum(nums)<S: return 0
+        dic = {0:1}
+        for i in range(len(nums)):
+            temp_dic = collections.defaultdict(int)
+            for k in dic:
+                temp_dic[k+nums[i]] += dic[k]
+                temp_dic[k-nums[i]] += dic[k]
+            dic = temp_dic
+        return dic[S]
         
 # @lc code=end
 if __name__ == '__main__':
